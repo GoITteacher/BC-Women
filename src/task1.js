14c13d44d316e47b1a316ec913b7ca405444d721
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-12 13:59:42 +0200

    update

--- src/task1.js
+++ src/task1.js
@@ -5,16 +5,6 @@
 //   { name: 'Щебень', price: 200, quantity: 2 },
 // ];
 
-// function calcTotalPrice(stones, stoneName) {
-//   for (let i = 0; i < stones.length; i++) {
-//     if (stones[i].name === stoneName) {
-//       return stones[i].price * stones[i].quantity;
-//     }
-//   }
-
-//   return null;
-// }
-
 // const res = calcTotalPrice(stones, 'Сапфир');
 
 // console.log(res);
