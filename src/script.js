a478833b9bca88dc68e17285ddc76592240aa8ff
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-13 13:31:25 +0200

    update

--- src/script.js
+++ src/script.js
@@ -3,20 +3,16 @@
 /* Перепиши функцию так, чтобы она принимала один объект параметров, вместо набора
 независимых аргументов. */
 
-/* function calcBMI({ weight = '1', height = '1' } = {}) {
-  const numericWeight = Number(weight.replace(',', '.'));
-  const numericHeight = Number(height.replace(',', '.'));
-  return Number((numericWeight / numericHeight ** 2).toFixed(1));
-}
+// function calcBMI(weight, height) {}
 
-console.log(calcBMI('88,3', '1.75'));
+// console.log(calcBMI('88,3', '1.75'));
 
-const obj = {
-  weight: '88,3',
-  height: '1.75',
-};
+// const obj = {
+//   weight: '88,3',
+//   height: '1.75',
+// };
 
-console.log(calcBMI(obj)); */
+// console.log(calcBMI(obj.weight, obj.height));
 
 // =======================
 
@@ -28,20 +24,13 @@ console.log(calcBMI(obj)); */
   paramsPhones: '89001234567,89001112233,890055566377,890055566300',
 };
 
-function printContactsInfo({ paramsNames: names = '', phones = '' } = {}) {
-  names = names.split(',');
-  phones = phones.split(',');
+function printContactsInfo(names, phones) {
   for (let i = 0; i < names.length; i++) {
     console.log(names[i], phones[i]);
   }
 }
 
-// Было
-// printContactsInfo('', '');
-
-// Ожидается
-
-printContactsInfo(params);
+printContactsInfo([],[]);
 printContactsInfo({}); */
 
 // =======================================
@@ -50,11 +39,10 @@ printContactsInfo({}); */
 независимых аргументов.
  */
 
-/* function getBotReport1(obj) {
-  const { companyName, repairBots, defenceBots } = obj;
+/* function getBotReport1(companyName, repairBots, defenceBots) {
   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 }
-function getBotReport2({ companyName, repairBots, defenceBots } = {}) {
+function getBotReport2(companyName, repairBots, defenceBots) {
   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 }
 
@@ -83,8 +71,8 @@ console.log(
   getStockReport({
     companyName: 'Cyberdyne Systems',
     stock: {
-      shoe234s: 20,
-      ha4ts: 26,
+      shoes: 20,
+      hats: 26,
     },
   }),
 ); */
@@ -97,21 +85,13 @@ console.log(
 объект контакта с добавленными свойствами `id` и `createdAt`, а также `list` со
 значением "default" если в `partialContact` нет такого свойства.
 
-1. create a function +
+1. create a function 
 2. create a resul object
 3. add props to object
 4. add list if prtialsContact doesn't have a property default
 */
 /* 
-function createContact(obj) {
-  const resultObj = {
-    id: 'someid',
-    createAt: 'some param',
-    list: 'default',
-    ...obj,
-  };
-  return resultObj;
-}
+
 
 console.log(
   createContact({
@@ -138,26 +118,16 @@ console.log(
 /* Напиши функцию `transformUsername(user)` так, чтобы она возвращала новый обьект
 со свойством `fullName`, вместо `firstName` и `lastName`. */
 
-/* function transformUsername({ firstName, lastName }) {
-  const obj = {
-    fullName: firstName + lastName,
-  };
-
-  return obj;
-} */
+const user = {
+  firstname: 'Петрик',
+  lastname: 'Пяточкин',
+};
 
 // =======================
 
 // Напишите функцию, которая возвращает вложенный массив вида [[key, value], [key, value]]
 
-/* function doubleArray(obj) {
-  let arr = [];
-  for (let key of Object.keys(obj)) {
-    arr.push([key, obj[key]]);
-  }
-
-  return arr;
-}
+/* 
 
 const user = {
   name: 'Mango',
@@ -169,41 +139,12 @@ console.log(doubleArray(user)); */
 // ==================================
 
 /* 
-Напишите функцию, которая возвращает новый объект без указанных значений.
+Напишите функцию, которая возвращает новый объект из указанных значений.
 */
-
-// function createObj(...props) {
-//   let newObj = {};
-
-//   for (let i = 0; i < props.length; i++) {
-//     const newKey = `prop${i + 1}`;
-//     newObj[newKey] = props[i];
-//   }
-//   console.log(newObj);
-// }
-
-// function createObj(...props) {
-//   let newObj = {};
-//   let index = 0;
-//   for (let value of props) {
-//     const newKey = `prop${index++ + 1}`;
-//     newObj[newKey] = value;
-//   }
-//   console.log(newObj);
-// }
 
 // createObj('234', [], {}, true, 2, 4, 2, 5, 1, 43, 5, 1);
 
 // ===============================
-
-// function createObj(arr, ...other) {
-//   const newObj = {};
-//   for (let i = 0; i < arr.length; i++) {
-//     const key = arr[i];
-//     newObj[key] = other[i];
-//   }
-//   console.log(newObj);
-// }
 
 // createObj(['prop1', 'prop2', 'prop3', 'prop4', 'prop5', 'prop6'], [], {});
 
@@ -211,38 +152,10 @@ console.log(doubleArray(user)); */
 
 // Напишите функцию, которая делает поверхностную проверку объекта на пустоту.
 
-// function trueObject(obj) {
-//   const values = Object.values(obj);
-//   for (const x of values) {
-//     if (!x || x.length === 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
 // const obj = {
 //   age: 123,
 //   name: {}, //null, '', []
 // };
-
-// =============================
-
-// const arr = [];
-
-// arr.toString = () => {
-//   console.log('HELLO');
-//   return 'test';
-// };
-
-// let num = 'awd';
-
-// num.t = () => {
-//   console.log('This is num');
-//   return 'test num';
-// };
-
-// console.log(num);
 
 // =================================
 
@@ -250,45 +163,14 @@ console.log(doubleArray(user)); */
     Напишите функцию, которая поверхностно сравнивает два объекта.
 */
 
-// function isEqual(obj1, obj2) {
-//   const keys1 = Object.keys(obj1);
-//   const keys2 = Object.keys(obj2);
-//   if (keys1.length !== keys2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < keys1.length; i++) {
-//     if (keys1[i] !== keys2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// function isEqual2(obj1, obj2) {
-
-//   for (let x of obj1) {
-//     if(!obj2.hasOwnProperty(x)){
-//         return false;
-//     }
-//   }
-//   return true;
-// }
-
 // =============================
 
 /* 
 Напишите функцию, которая вызывает метод массива на заданный путь объекта.
 */
 
-/* function getProps(path, obj) {
-  const pathArray = path.split(',');
-  console.log(pathArray);
-  for (let key of pathArray) {
-    obj = obj[key];
-    console.log(obj);
-  }
-  return obj;
-}
+/* 
+
 
 const obj = {
   FIO: {
@@ -312,18 +194,5 @@ getProps('addres,street,house', obj); */
 // =======================
 
 /* 
-// Напишите функцию, которая поверхностно находит пересечения объектов и возвращает объект пересечений.
+Напишите функцию, которая поверхностно находит пересечения объектов и возвращает объект пересечений.
 */
-
-// 1. function
-
-function crossObjects(obj1, obj2) {
-  const totalObj = {};
-
-  for (const key of Object.keys(obj1)) {
-    if (obj2.hasOwnProperty(key)) {
-      totalObj[key] = obj1[key];
-    }
-  }
-  return totalObj;
-}
