419bac6e29f1bd5d79ccc73cd890fa6b21eb7fc9
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-15 17:52:24 +0200

    update

--- src/script.js
+++ src/script.js
@@ -156,6 +156,19 @@ onSale которых true.
 /* Пусть функция `getTotalAmount` возвращает общее количество автомобилей(значение
 свойств `amount`). */
 
+const getTotalAmount = cars => {
+  let totalAmount = 0;
+
+  cars
+    .filter(car => car.onSale)
+    .filter((value, index) => index % 2 === 0)
+    .forEach(car => {
+      totalAmount += car.amount;
+    });
+
+  return totalAmount;
+};
+
 // console.log(getTotalAmount(cars));
 
 // ======================================================
@@ -163,30 +176,148 @@ onSale которых true.
 /* Пусть функция `getAvailableCarNames` возвращает массив моделей автомобилей, но
 только тех, которые сейчас на распродаже. */
 
+const getAvailableCarNames = cars => {
+  return cars
+    .filter(car => car.onSale)
+    .map(car => car.model)
+    .sort((a, b) => a.localeCompare(b));
+};
+
 // console.table(getAvailableCarNames(cars));
 
-// ======================================================
+// =====================================================
 /* 
 Пусть функция `getSortedCarsOnSale` возвращает массив автомобилей на распродаже
 (свойство onSale), отсортированных по возрастанию цены. */
 
-// console.table(getSortedCarsOnSale(cars));
+let counter = 0;
+
+const getSortedCarsOnSale = cars => {
+  return [...cars]
+    .sort((a, b) => {
+      console.log(counter++);
+      return a.price - b.price;
+    })
+    .filter(car => car.onSale);
+};
+
+const getSortedCarsOnSale1 = cars => {
+  return cars
+    .filter(car => car.onSale)
+    .sort((a, b) => {
+      console.log(counter++);
+      return a.price - b.price;
+    });
+};
+
+// console.table(getSortedCarsOnSale1(cars));
+
+// 10 => 111
+// 10 => 14
 
 // ======================================================
 
 // https://www.codewars.com/kata/5983cba828b2f1fd55000114
-
-// https://www.codewars.com/kata/59df2f8f08c6cec835000012
-
-// https://www.codewars.com/kata/5783ef69202c0ee4cb000265
-
-// https://www.codewars.com/kata/60cc93db4ab0ae0026761232
-
-// https://www.codewars.com/kata/513e08acc600c94f01000001
-
-// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+const oddOne = arr => arr.findIndex(el => el % 2 !== 0);
 
 // ======================================================
+// https://www.codewars.com/kata/59df2f8f08c6cec835000012
+s =
+  'Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill';
+
+function meeting(s) {
+  const users = s
+    .split(';')
+    .map(user => {
+      const [firstname, lastname] = user.split(':');
+      const objectUser = {
+        firstname: firstname.toUpperCase(),
+        lastname: lastname.toUpperCase(),
+      };
+      return objectUser;
+    })
+    .sort((a, b) => {
+      if (a.lastname !== b.lastname) {
+        return a.lastname.localeCompare(b.lastname);
+      } else {
+        return a.firstname.localeCompare(b.firstname);
+      }
+    })
+    .map(user => `(${user.lastname}, ${user.firstname})`)
+    .join('');
+
+  return users;
+}
+
+function meeting2(s) {
+  return s
+    .toUpperCase()
+    .split(';')
+    .sort((str1, str2) => {
+      str1 = str1.split(':'); // ["Fred", "Corwil"]
+      str2 = str2.split(':'); // ["Adam", "Corwil"]
+
+      if (str1[1] === str2[1]) {
+        return str1[0].localeCompare(str2[0]);
+      }
+
+      return str1[1].localeCompare(str2[1]);
+    })
+    .map(str => {
+      const [firstname, lastname] = str.split(':');
+      return `(${lastname}, ${firstname})`;
+    });
+}
+
+console.log(meeting2(s));
+
+// ======================================================
+// https://www.codewars.com/kata/5783ef69202c0ee4cb000265
+
+// ======================================================
+// https://www.codewars.com/kata/60cc93db4ab0ae0026761232
+
+S = [1, 2, 3, 4, 5, 6, 7, 8]; // 5,2,3,4,6,undefined
+
+// function arrange(s) {
+//   const T = [];
+//   while (S.length > 0) {
+//     T.push(S.shift());
+
+//     if (S.length > 0) T.push(S.pop());
+
+//     S.reverse();
+//   }
+//   return T;
+// }
+
+function arrange(s) {
+  const T = [];
+
+  for (let i = 0; i < s.length / 2; i++) {
+    const first = s[i];
+    const last = s[s.length - 1 - i];
+
+    if (i % 2 === 0) {
+      T.push(first, last);
+    } else {
+      T.push(last, first);
+    }
+  }
+
+  if (S.length % 2 !== 0) {
+    T.pop();
+  }
+  return T;
+}
+
+console.log(arrange(S));
+// ======================================================
+// https://www.codewars.com/kata/513e08acc600c94f01000001
+// ======================================================
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+// ======================================================
+// =====================================================
 
 // function encryptThis(text) {
 //   return text.split(' ').map(encryptWord);
